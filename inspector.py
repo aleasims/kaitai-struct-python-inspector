fece66856a811bf1bad76577cdffbadf7f51e1b9
Alexander Evgin <aleasims@gmail.com>
2019-09-03 16:10:53 +0300

    Fixed Trawl-JSON serialization

--- inspector.py
+++ inspector.py
@@ -1,18 +1,21 @@
+import base64
 import functools
 import json
+from enum import Enum
 from queue import Queue
 
 from graphviz import Digraph
 
-import parsetree
 import ksy
+import parsetree
 
-TRAWL_TYPES = {
-    'RootNode': 'ROOT',
-    'StructNode': 'STRUCTURE',
-    'ValueNode': 'LEAF',
-    'ArrayNode': 'SEQUENCE'
-}
+
+class NodeType(Enum):
+    ValueNode = 0  # LEAF
+    StructNode = 1  # STRUCTURE
+    ArrayNode = 2  # SEQUENCE
+    RootNode = 3  # ROOT
+    VariantNode = 4  # VARIANT
 
 
 def with_tree_only(func):
@@ -59,26 +62,31 @@ class Inspector:
     def to_trawl(self):
         def _to_trawl(node, **kwargs):
             """Recursive callable."""
-            json = kwargs.copy()
+            tjson = kwargs.copy()
             childs = getattr(node, 'childs', [])
             if childs:
-                json['fields'] = {}
+                tjson['fields'] = {}
                 for i, child in enumerate(childs):
                     args = {
                         'position': i,
                         'length': child.size,
                         'offset': child.start,
-                        'type': TRAWL_TYPES[child.__class__.__name__]
+                        'type': getattr(NodeType, child.__class__.__name__).value
                     }
                     if isinstance(child, parsetree.ValueNode):
                         args['interpreted_value'] = str(child.value)
                         args['value_type'] = child.value.__class__.__name__
                     child_json = _to_trawl(child, **args)
-                    json['fields'][child.name] = child_json
-            return json
+                    tjson['fields'][child.name] = child_json
+            return tjson
 
-        return {self.tree.name: _to_trawl(self.tree,
-                                          position=0,
-                                          offset=0,
-                                          length=self.tree.size,
-                                          type=TRAWL_TYPES['RootNode'])}
+        return json.dumps({'format': _to_trawl(self.tree,
+                                               position=0,
+                                               offset=0,
+                                               length=self.tree.size,
+                                               type=NodeType.RootNode.value),
+                            'meta': {
+                                'generic': False,
+                                'buffer': base64.b64encode(self.tree.buffer).decode()
+                            }
+                          })
