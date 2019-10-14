ab868cb49e858f4c3c3cb38222a6dc974766054d
aleasims <aleasims@gmail.com>
2019-10-14 19:54:43 +0300

    Refactored installation

--- setup.py
+++ setup.py
@@ -1,3 +1,11 @@
+"""
+    Development installation:
+pip install -e .[dev]
+
+    Normal installation
+pip install .
+"""
+
 import os
 
 from setuptools import setup
@@ -9,6 +17,30 @@ here = os.path.abspath(os.path.dirname(__file__))
 with open(os.path.join(here, 'README.md'), encoding='utf-8') as f:
     long_description = f.read()
 
+requierments = [
+    'kaitaistruct>=0.8',
+    'graphviz>=0.11.1',
+    'pyyaml>=5.1.1'
+]
+
+requierments_dev = [
+    'pylint',
+    'flake8',
+    'autopep8'
+]
+
+data_paths = {'kspyspector': ['tests/data/*']}
+entry_points = {'console_scripts': ['kspyspector=scripts.main:main']}
+
+classifiers = '''
+Development Status :: 3 - Alpha
+Programming Language :: Python :: 3
+Programming Language :: Python :: 3.5
+Programming Language :: Python :: 3.6
+Programming Language :: Python :: 3.7
+License :: OSI Approved :: GNU General Public License v3 or later (GPLv3+)
+'''.rstrip().lstrip().split('\n')
+
 
 setup(
     name='kspyspector',
@@ -18,29 +50,13 @@ setup(
     long_description_content_type='text/markdown',
     url='https://github.com/aleasims/kaitai-struct-python-inspector',
     author=kspyspector.__author__,
-    classifiers=[
-        'Development Status :: 3 - Alpha',
-        'Programming Language :: Python :: 3',
-        'Programming Language :: Python :: 3.5',
-        'Programming Language :: Python :: 3.6',
-        'Programming Language :: Python :: 3.7',
-        'License :: OSI Approved :: GNU General Public License v3 or later (GPLv3+)'
-    ],
+    classifiers=classifiers,
     keywords='kaitaistruct parsetree',
     packages=['kspyspector'],
     package_dir={'kspyspector': 'kspyspector'},
     python_requires='>=3.5',
-    install_requires=[
-        'kaitaistruct>=0.8',
-        'graphviz>=0.11.1',
-        'pyyaml>=5.1.1'
-    ],
-    package_data={
-        'kspyspector': ['tests/data/*'],
-    },
-    entry_points={
-        'console_scripts': [
-            'kspyspector=scripts.main:main',
-        ]
-    }
+    install_requires=requierments,
+    extras_require={'dev': requierments_dev},
+    package_data=data_paths,
+    entry_points=entry_points
 )
