292e78fe81b0050beb0a1c36a353503621c8109a
aleasims <aleasims@gmail.com>
2019-10-16 15:29:34 +0300

    Refactored ksy module

--- kspyspector/ksy.py
+++ kspyspector/ksy.py
@@ -1,49 +1,94 @@
-import importlib.util
-import inspect
 import json
 import os
-import shutil
 import subprocess
-import time
 
 import yaml
+
+from kspyspector import utils
 
 
 class KsyError(Exception):
     pass
 
 
-class TmpDir:
+class Ksy:
+    """Represent ksy file.
+
+    Attrs:
+        path - path to ksy file
+
+    Methods to use:
+        interpret()
+        compile()
+
+    After compilation, following attrs will be set:
+        spec_name - id of the format specified in the ksy
+        module - created module
+        parser - created parser from module
+    """
+
     def __init__(self, path):
         self.path = path
 
-    def __enter__(self):
-        os.makedirs(self.path, exist_ok=True)
-        return self.path
+    def interpret(self):
+        """Return loaded version of ksy."""
 
-    def __exit__(self, exc_type, exc_value, exc_traceback):
-        shutil.rmtree(self.path)
+        return yaml.safe_load_all(open(self.path))
+
+    def compile(self, outdir=None, clear=True):
+        """Compile ksy with ksc.
+
+        Args:
+            outdir - path to dir, where to store created module
+            clear - clear output directory at the end
+
+        Created module can be saved or deleted at the end.
+
+        Return parser class.
+        """
+
+        outdir = outdir if outdir is not None else utils.tmpdir()
+        output = call_ksc(self.path, 'python', outdir=outdir, debug=True,
+                          ksc_json_output=True, verbose=True)
+
+        class_name, module_filename = self._parse_output(output)
+        module_path = os.path.join(outdir, module_filename)
+        self.module = utils.import_module(self.spec_name, module_path)
+
+        self.parser = utils.get_class(self.module, class_name)
+
+        if clear:
+            if os.path.exists(self.module.__file__):
+                os.remove(self.module.__file__)
+            del outdir
+
+        return self.parser
+
+    def _parse_output(self, output):
+        """Extract important fields from ksc output."""
+
+        result = json.loads(output)[self.path]
+
+        self.spec_name = result['firstSpecName']
+        mod_params = result['output']['python'][self.spec_name]
+
+        return (mod_params['topLevelName'],
+                mod_params['files'].pop()['fileName'])
 
 
-def create_tmp():
-    return TmpDir('/tmp/kaitai/{pid}_{time}'.format(
-        pid=os.getpid(), time=int(time.time())))
+def call_ksc(ksy_path, target, outdir='.', debug=False,
+             ksc_json_output=False, verbose=False):
+    """Run kaitaistruct compiler in subprocess with params.
 
+    Return the output of process.
+    """
 
-def import_module(name, path):
-    spec = importlib.util.spec_from_file_location(name, path)
-    module = importlib.util.module_from_spec(spec)
-    spec.loader.exec_module(module)
-    return module
+    cmd = ['ksc', '--target', target, '--outdir', outdir, ksy_path]
+    if debug:
+        cmd += ['--debug']
+    if ksc_json_output:
+        cmd += ['--ksc-json-output']
 
-
-def compile_ksy(path, tmp, verbose=False):
-    cmd = ['ksc',
-           '--ksc-json-output',
-           '--debug',
-           '--target', 'python',
-           '--outdir', tmp,
-           path]
     if verbose:
         print(' '.join(cmd))
 
@@ -52,36 +97,9 @@ def compile_ksy(path, tmp, verbose=False):
                             stderr=subprocess.PIPE)
     out, err = proc.communicate()
     if err:
-        print('Something went wrong during compilation!')
         raise KsyError(err.decode('utf-8'))
+
+    if verbose:
+        print(out)
+
     return out
-
-
-def parse_result(result, ksy_path, module_dir):
-    result = json.loads(result)[ksy_path]
-    target_lang = 'python'
-
-    module_name = result['firstSpecName']
-    module_spec = result['output'][target_lang][module_name]
-    module_filename = module_spec['files'].pop()['fileName']
-    module_path = os.path.join(module_dir, module_filename)
-
-    class_name = module_spec['topLevelName']
-
-    return module_name, module_path, class_name
-
-
-def get_class(module, name):
-    for objname, obj in inspect.getmembers(module):
-        if inspect.isclass(obj) and objname == name:
-            return obj
-    raise ImportError('Parser class not found in compiled code.')
-
-
-def compile(path, verbose=False):
-    with create_tmp() as tmp:
-        result = compile_ksy(path, tmp, verbose)
-        module_name, module_path, class_name = parse_result(result, path, tmp)
-        module = import_module(module_name, module_path)
-        Parser = get_class(module, class_name)
-    return Parser
